428d1b0a5abff8e0a57ea5cb5d1c7e5b8b619080
Erik Abair <erik.abair@gmail.com>
2021-12-20 09:21:26 -0800

    Adds material alpha test output.

---
+++ collect.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+
+readonly test_output_dir='e:\nxdk_pgraph_tests\'
+
+if [[ $# -gt 0 ]]; then
+  xbox=$1
+else
+  if [[ -z "${XBOX:-}" ]]; then
+    echo "Usage: $0 <xbox_ip_address>"
+    echo ""
+    echo "Alternatively, set the XBOX environment variable to your XBOX ip address."
+    exit 1
+  fi
+
+  xbox="${XBOX}"
+fi
+
+
+echo "Collecting output of nxdk_pgraph_tests from ${xbox}..."
+xbdm "${xbox}" getfile "${test_output_dir}" .
+
